9750ddc76e66bf7e5b5c9b8e0ff499e512cbee0a
Yang <yrzhang@thoughtworks.com>
2023-01-02 18:52:08 +1100

    show case what is OpenTelemetry

---
+++ DistributedTracing.CollectTraces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static ActivitySource s_source = new ActivitySource("Sample.DistributedTracing");
+
+    static async Task Main(string[] args)
+    {
+        Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+        Activity.ForceDefaultIdFormat = true;
+
+        Console.WriteLine("         {0,-15} {1,-60} {2,-15}", "OperationName", "Id", "Duration");
+        ActivitySource.AddActivityListener(new ActivityListener()
+        {
+            ShouldListenTo = (source) => true,
+            Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllDataAndRecorded,
+            ActivityStarted = activity => Console.WriteLine("Started: {0,-15} {1,-60}", activity.OperationName, activity.Id),
+            ActivityStopped = activity => Console.WriteLine("Stopped: {0,-15} {1,-60} {2,-15}", activity.OperationName, activity.Id, activity.Duration)
+        });
+
+        await DoSomeWork();
+        Console.WriteLine("Example work done");
+    }
+
+    static async Task DoSomeWork()
+    {
+        using (Activity a = s_source.StartActivity("SomeWork"))
+        {
+            await StepOne();
+            await StepTwo();
+        }
+    }
+
+    static async Task StepOne()
+    {
+        using (Activity a = s_source.StartActivity("StepOne"))
+        {
+            await Task.Delay(500);
+        }
+    }
+
+    static async Task StepTwo()
+    {
+        using (Activity a = s_source.StartActivity("StepTwo"))
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
--- src/DistributedTracing.Console/Program.cs
+++ src/DistributedTracing.Console/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+class Program
+{
+    static ActivitySource s_source = new ActivitySource("Sample.DistributedTracing");
+
+    public static async Task Main()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MySample"))
+            .AddSource("Sample.DistributedTracing")
+            .AddConsoleExporter()
+            .Build();
+
+        await DoSomeWork();
+        Console.WriteLine("Example work done");
+    }
+
+    static async Task DoSomeWork()
+    {
+        using (Activity a = s_source.StartActivity("SomeWork"))
+        {
+            await StepOne();
+            await StepTwo();
+        }
+    }
+
+    static async Task StepOne()
+    {
+        using (Activity a = s_source.StartActivity("StepOne"))
+        {
+            await Task.Delay(500);
+        }
+    }
+
+    static async Task StepTwo()
+    {
+        using (Activity a = s_source.StartActivity("StepTwo"))
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
